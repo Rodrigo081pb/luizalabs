41f046234eaa2330c9f328d8f4aae015223f1454
KAUA RODRIGO PINHEIRO BRAGA <kauarodrigo1193@gmail.com>
2025-12-22 22:29:57 -0300

    subindo arquivos

--- manipulando_arquivos/write.py
+++ manipulando_arquivos/write.py
@@ -1,5 +1,6 @@
 arquivo = open('C:/Users/r.pinheiro.braga/Desktop/luizalabs/manipulando_arquivos/src/archive_2.txt', 'w', encoding='utf-8')
 
 arquivo.write("Olá, mundo!")
+arquivo.write("\n um novo texto nele, clarinho que sim")
 
 arquivo.close()
