ff9f0f570edbe1b552f1128161758eeeb724f485
KAUA RODRIGO PINHEIRO BRAGA <kauarodrigo1193@gmail.com>
2025-12-22 22:49:11 -0300

    feat: criando arquivo dentro do diretório

--- manipulando_arquivos/gerenciarArquivesAndDiretorios.py
+++ manipulando_arquivos/gerenciarArquivesAndDiretorios.py
@@ -4,11 +4,15 @@ import shutil
 # criar um diretório
 os.mkdir('exemplo')
 
+# criar um arquivo dentro do diretório
+with open('exemplo/exemplo.txt', 'w', encoding='utf-8') as f:
+    f.write('Conteúdo de exemplo')
+
 # Renomear um diretório
 os.rename('exemplo.txt', 'exemplo_renomeado.txt')
 
 # Remover arquivo
-os.remove('exemplo_renomeado.txt')
+os.remove('exemplo/exemplo.txt')
 
 # mover um diretório
 shutil.move('diretorio_origem.txt', 'diretorio_destino.txt')
