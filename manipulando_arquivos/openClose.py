44be5acc388e218b7b47cc43a5b5e015f13f1742
KAUA RODRIGO PINHEIRO BRAGA <kauarodrigo1193@gmail.com>
2025-12-22 21:39:48 -0300

    feat: subindo os arquivos

--- manipulando_arquivos/openClose.py
+++ manipulando_arquivos/openClose.py
@@ -1,6 +1,11 @@
 # Abrindo arquivos no modo Read
-
 arquivo = open('./src/archive.txt', 'r')
+
+# Abrindo arquivos no modo Write
+arquivo = open('./src/archive.txt', 'w')
+
+# Abrindo arquivos no modo Append
+arquivo = open('./src/archive.txt', 'a')
 
 # fechando o arquivo
 arquivo.close()
