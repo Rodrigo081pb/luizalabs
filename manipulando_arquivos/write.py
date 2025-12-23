af49277b48b1cd72423e8015f73df53ebd30e4b5
KAUA RODRIGO PINHEIRO BRAGA <kauarodrigo1193@gmail.com>
2025-12-22 22:32:09 -0300

    escrevendo mais através do método wiritelines

--- manipulando_arquivos/write.py
+++ manipulando_arquivos/write.py
@@ -4,3 +4,11 @@ arquivo.write("Olá, mundo!")
 arquivo.write("\n um novo texto nele, clarinho que sim")
 
 arquivo.close()
+
+# Escrevendo em arquivos de texto
+arquivo = open('C:/Users/r.pinheiro.braga/Desktop/luizalabs/manipulando_arquivos/src/archive_2.txt', 'w', encoding='utf-8')
+
+arquivo.write('\n um nova linha')
+arquivo.write('\n um nova linha')
+arquivo.write('\n um nova linha')
+arquivo.write('\n um nova linha')
